ce29ccc38180b95e0a0c0b1a763892b75c6bb3a2
bazoaltoSamuel <bazoaltoSamuel33@outlook.com>
2021-12-14 12:14:17 -0400

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'pdfMakerDemo';
+export class AppComponent implements OnInit{
+
+  title = new FormControl("This a title example", Validators.required)
+  content = new FormControl("Se puede aplicar estilos a estas secciones, como tamanho color posicion entre otras cosas", Validators.required)
+
+  ngOnInit(): void {
+    
+  }
+
+  download(){
+    const document = {
+      content: [
+        this.title.value,
+        {text: this.content.value, fontSize: 25},
+        {
+          // to treat a paragraph as a bulleted list, set an array of items under the ul key
+          ul: [
+            'Item 1',
+            'Item 2',
+            'Item 3',
+            { text: 'Item 4', bold: true },
+          ]
+        },
+    
+        'Numbered list example:',
+        {
+          // for numbered lists set the ol key
+          ol: [
+            'Item 1',
+            'Item 2',
+            'Item 3'
+          ]
+        }
+      ]
+    }
+    pdfMake.createPdf(document).download()
+  }
+
+  open(){
+    const document = {
+      content: [
+        this.title.value,
+        {text: this.content.value, fontSize: 25, color: 'red'},
+        {
+          // to treat a paragraph as a bulleted list, set an array of items under the ul key
+          ul: [
+            'Item 1',
+            'Item 2',
+            'Item 3',
+            { text: 'Item 4', bold: true },
+          ]
+        },
+    
+        'Numbered list example:',
+        {
+          // for numbered lists set the ol key
+          ol: [
+            'Item 1',
+            'Item 2',
+            'Item 3'
+          ]
+        }
+      ]
+    }
+    pdfMake.createPdf(document).open()
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatProgressBarModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
